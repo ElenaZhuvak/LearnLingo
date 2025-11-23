f96cc02f11d5d14143802e4ffdf951c7e5498349
Elena Zhuvak <elenkazh@gmail.com>
2025-11-23 19:36:02 +0000

    feat: implement Google sign-in and logout functionality

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import { auth } from '../../firebase.js';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth, googleProvider } from '../../firebase-config.js';
+import { createUserWithEmailAndPassword, signInWithPopup, signOut} from 'firebase/auth';
 
 const Auth = () => {
     const [email, setEmail] = useState();
@@ -15,11 +15,37 @@ const Auth = () => {
         
     };
 
+    console.log(auth?.currentUser?.email);
+
+// * Sign In with Google 
+    const signInWithGoogle = async () => {
+        try {
+            await signInWithPopup(auth, googleProvider);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    console.log(auth?.currentUser?.photoURL);
+
+///////////////////
+
+    const logout = async () => {
+        try {
+            await signOut(auth);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+
   return (
     <div>
         <input type="email" placeholder="Email" onChange={e => setEmail(e.target.value)} />
         <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
+
         <button onClick={signIn}>Sign In</button>
+        <button onClick={signInWithGoogle}>Sign In With Google</button>
+        <button onClick={logout}>Logout</button>
     </div>
   );
 };
